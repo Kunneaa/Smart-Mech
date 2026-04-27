9a77a2c415a815f9da05aaaec370331a55b0b89a
Hilo22 <hieup0706@gmail.com>
2026-04-27 13:52:26 +0700

    UI for Recommendation monitor

--- frontend/components/EngineCard.jsx
+++ frontend/components/EngineCard.jsx
@@ -4,9 +4,14 @@ import DisplayResult from './DisplayResult'
 import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 const EngineCard = (props) => {
-  const {kieu_dong_co, cong_suat, van_toc_vong_quay, onSelect, isSelected} = props;
+  const {kieu_dong_co, cong_suat, van_toc_vong_quay, onSelect, isSelected, isMostSuggested} = props;
   return (
     <View style={styles.engineBlock}>
+      {isMostSuggested && (
+        <View style={styles.badgeMostSuggested}>
+          <Text style={styles.badgeText}>TOP RECOMMENDATION</Text>
+        </View>
+      )}
       <DisplayResult variable={"Kiểu động cơ"} value={kieu_dong_co} unit={""}/>
       <DisplayResult variable={"Công suất"} value={cong_suat} unit={"kW"}/>
       <DisplayResult variable={"Vận tốc quay"} value={van_toc_vong_quay} unit={"vg/ph"}/>
@@ -43,5 +48,22 @@ const styles = StyleSheet.create({
     borderRadius:10,
     width:50,
     textAlign:'center',
+  },
+  badgeMostSuggested: {
+    backgroundColor: 'transparent',
+    borderLeftWidth: 4,
+    borderLeftColor: '#C0A080',
+    borderRightWidth: 4,
+    borderRightColor: '#C0A080',
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    marginBottom: 10,
+  },
+  badgeText: {
+    
+    fontFamily: 'quicksand-bold',
+    fontSize: 11,
+    color: '#C0A080',
+    letterSpacing: 0.5,
   },
 })
